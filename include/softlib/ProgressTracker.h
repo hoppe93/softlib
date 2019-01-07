88ea5e92c8ffaf3d14bba0e998f51087fc100c66
Mathias Hoppe <hoppe@chalmers.se>
2019-01-07 09:56:53 +0100

    Implemented progress tracker

--- include/softlib/ProgressTracker.h
+++ include/softlib/ProgressTracker.h
@@ -11,44 +11,56 @@ class ProgressTracker {
         enum ProgressType {
             PROGRESS_LINES
         };
-
+        enum TimeUnit {
+            DAYS=1,
+            HOURS=2,
+            MINUTES=3,
+            SECONDS=4,
+            MILLISECONDS=5
+        };
     private:
         Timer *start, *last;
 
         bool colors = false;    // If true, output is formatted with fancy colors
         bool estimate_remaining = false;    // Estimate how much time remains
-        size_t dprint,          // Number of counts per progress point
-               nprint,          // Number of progress points to print
-               modlevel,        // If (total%nprint > 0), we add 1 when
-                                // incrementing 'next' until this value
-                                // has been reached
-               total,           // Total number of progress points
-               next,            // Next point at which to emit progress
-               nextindex;       // Index (from 1 to nprint) of next point
+        long long int
+            dprint,          // Number of counts per progress point
+            nprint,          // Number of progress points to print
+            modlevel,        // If (total%nprint > 0), we add 1 when
+                             // incrementing 'next' until this value
+                             // has been reached
+            total,           // Total number of progress points
+            next,            // Next point at which to emit progress
+            nextindex;       // Index (from 1 to nprint) of next point
+
+        // Running times of three previous blocks
+        //   dt1 = t_{i-1} - t_{i-2}
+        //   dt2 = t_{i-2} - t_{i-3}
+        long long int dt1, dt2;
 
         unsigned int ndigits;   // Number of digits in 'nprint'
 
         ProgressType type = PROGRESS_LINES;
     public:
         ProgressTracker(
-            const size_t, const size_t, const ProgressType,
+            const long long int, const long long int, const ProgressType,
             const bool colors=false, const bool estimate=false
         );
         ~ProgressTracker();
 
-        void PrintProgress(const size_t);
-
-        void PrintProgressBar();
-        void PrintProgressLines();
+        void PrintProgress(const long long int);
+        void PrintProgressLines(const long long int);
 
         void UpdateNext();
 
-        size_t DiffPassedTime();
-        size_t EstimateRemainingTime();
-        size_t TotalPassedTime();
-        size_t PassedTime(Timer*);
+        long long int DiffPassedTime();
+        long long int EstimateRemainingTimeAverage(const long long int);
+        long long int EstimateRemainingTimeDeterministic(const long long int);
+        long long int TotalPassedTime();
+        long long int PassedTime(Timer*);
 
-        std::string TimeString(const size_t);
+        std::string TimeString(const long long int, const bool color=false, const TimeUnit tu=TimeUnit::HOURS);
+        std::string FormattedTimeString(const long long int, const long long int, const long long int, const long long int, const long long int, const bool color=false);
 };
 
 #endif/*_SOFTLIB_PROGRESS_TRACKER_H*/
--- src/General/ProgressTracker.cpp
+++ src/General/ProgressTracker.cpp
@@ -10,8 +10,10 @@
  * 'COLOR_TERMINAL' defined.
  */
 
-#include <cstdio>
 #include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <omp.h>
 #include <sstream>
 #include <string>
 #include <softlib/config.h>
@@ -28,8 +30,8 @@ using namespace std;
  * colors: If true, enables colored terminal output.
  */
 ProgressTracker::ProgressTracker(
-    const size_t n, const size_t nprint, const ProgressType type,
-    const bool colors, const bool estimate
+    const long long int n, const long long int nprint,
+    const ProgressType type, const bool colors, const bool estimate
 ) {
     this->total = n;
     this->nprint = nprint;
@@ -71,74 +73,134 @@ void ProgressTracker::UpdateNext() {
 /**
  * Print progress (if necessary).
  */
-void ProgressTracker::PrintProgress(const size_t i) {
-    if (i >= this->next) {
-        if (this->type == PROGRESS_LINES)
-            PrintProgressLines();
+void ProgressTracker::PrintProgress(const long long int i) {
+    const long long int index = this->nextindex;
+    bool returnImmediately = false;
 
-        this->UpdateNext();
+    #pragma omp critical (ProgressTracker_Print)
+    {
+        if (i < this->next)
+            returnImmediately = true;
+        else
+            this->UpdateNext();
     }
+
+    if (returnImmediately)
+        return;
+
+    if (this->type == PROGRESS_LINES)
+        PrintProgressLines(index);
+
+    this->last->Reset();
 }
 
 /**
  */
-void ProgressTracker::PrintProgressLines() {
-    string remaining;
-    string timestring = TimeString(TotalPassedTime());
+void ProgressTracker::PrintProgressLines(const long long int index) {
+    string remaining, totalrem;
+    long long int totalpassed = TotalPassedTime();
+    string absolute = TimeString(totalpassed, colors);
+    string relative = TimeString(DiffPassedTime(), colors);
 
-    if (estimate_remaining)
-        remaining = TimeString(EstimateRemainingTime());
+    if (estimate_remaining) {
+        long long int est = EstimateRemainingTimeDeterministic(index);
+        //long long int est = EstimateRemainingTimeAverage(index);
+        remaining = TimeString(est, colors);
+        totalrem  = TimeString(est+totalpassed, colors);
+    }
 
     if (colors) {
         if (estimate_remaining)
             printf(
-                "\x1B[1;32mPROGRESS\x1B[0m (%zu/%zu) :: %s passed, estimated %s remaining\n",
-                this->nextindex, this->nprint,
-                timestring.c_str(), remaining.c_str()
+                "\x1B[1;32mPROGRESS\x1B[0m (%lld/%lld) :: %s / %s passed, %s remaining (%s total)\n",
+                index, this->nprint,
+                absolute.c_str(), relative.c_str(), remaining.c_str(), totalrem.c_str()
             );
         else
             printf(
-                "\x1B[1;32mPROGRESS\x1B[0m (%zu/%zu) :: %s passed\n",
-                this->nextindex, this->nprint,
-                timestring.c_str()
+                "\x1B[1;32mPROGRESS\x1B[0m (%lld/%lld) :: %s / %s passed\n",
+                index, this->nprint,
+                absolute.c_str(), relative.c_str()
             );
     } else {
         if (estimate_remaining)
             printf(
-                "PROGRESS (%zu/%zu) :: %s passed, estimated %s remaining\n",
-                this->nextindex, this->nprint,
-                timestring.c_str(), remaining.c_str()
+                "PROGRESS (%lld/%lld) :: %s / %s passed, %s remaining (%s total)\n",
+                index, this->nprint,
+                absolute.c_str(), relative.c_str(), remaining.c_str(), totalrem.c_str()
             );
         else
             printf(
-                "PROGRESS (%zu/%zu) :: %s passed\n",
-                this->nextindex, this->nprint,
-                timestring.c_str()
+                "PROGRESS (%lld/%lld) :: %s / %s passed\n",
+                index, this->nprint,
+                absolute.c_str(), relative.c_str()
             );
     }
 }
 
 /**
- * Estimate time remaining (assuming equal time
- * per progress point).
+ * Estimate time remaining after executing operation by
+ * averaging over previous durations.
  */
-size_t ProgressTracker::EstimateRemainingTime() {
-    size_t passed = TotalPassedTime();
-    size_t rem = (size_t)(((slibreal_t)passed) * (((slibreal_t)this->nprint)/((slibreal_t)this->nextindex)));
+long long int ProgressTracker::EstimateRemainingTimeAverage(const long long int i) {
+    long long int ti = TotalPassedTime();
+    long long int N = this->nprint;
 
-    return rem;
+    return (((N-i)*ti)/i);
+}
+
+/**
+ * Estimate time remaining after executing operation i,
+ * assuming the duration of each operation varies deterministically.
+ * Returns the number of remaining milliseconds.
+ */
+long long int ProgressTracker::EstimateRemainingTimeDeterministic(const long long int i) {
+    long long int dt = DiffPassedTime(), d2t=0, d3t=0, eT;
+    long long int Ni = this->nprint - i;
+    long long int NNp12, NNp1Np26;
+
+    eT = Ni * dt;
+
+    if (i >= 2) {
+        d2t = dt - this->dt1;
+
+        if (Ni%2==0) NNp12 = (Ni/2)*(Ni+1);
+        else NNp12 = Ni*((Ni+1)/2);
+
+        if (d2t*dt > 0)
+            eT += NNp12 * d2t;
+    }
+
+    /*if (i >= 3) {
+        d3t = (dt + this->dt2 - 2*this->dt1);
+
+        if (Ni%3==0)
+            NNp1Np26 = (Ni/3)*((Ni+1)/2)*(Ni+2);
+        else if ((Ni+1)%3==0)
+            NNp1Np26 = (Ni/2)*((Ni+1)/3)*(Ni+2);
+        else
+            NNp1Np26 = Ni*((Ni+1)/2)*((Ni+2)/3);
+
+        if (d2t*dt > 0)
+            eT += NNp1Np26 * d3t;
+    }*/
+
+    this->dt2 = this->dt1;
+    this->dt1 = dt;
+
+    return eT;
 }
 
 /**
  * Get the total time passed since this
  * object was initialized.
  */
-size_t ProgressTracker::TotalPassedTime() { return PassedTime(this->start); }
+long long int ProgressTracker::TotalPassedTime() { return PassedTime(this->start); }
 /**
  * Get the amount of time that passed since
  * the last progress point was reported.
  */
-size_t ProgressTracker::DiffPassedTime()  { return PassedTime(this->last); }
+long long int ProgressTracker::DiffPassedTime()  { return PassedTime(this->last); }
 
 /**
  * Compute the amount of time that has passed
@@ -146,11 +208,11 @@ size_t ProgressTracker::DiffPassedTime()  { return PassedTime(this->last); }
  *
  * time: Timer object representing the start time.
  */
-size_t ProgressTracker::PassedTime(Timer *tim) {
+long long int ProgressTracker::PassedTime(Timer *tim) {
     chrono::time_point<chrono::high_resolution_clock> clk = chrono::system_clock::now();
-    chrono::seconds sec = chrono::duration_cast<chrono::seconds>(clk - tim->GetClock());
+    chrono::milliseconds ms = chrono::duration_cast<chrono::milliseconds>(clk - tim->GetClock());
 
-    return ((size_t)sec.count());
+    return ((long long int)ms.count());
 }
 
 /**
@@ -170,55 +232,111 @@ size_t ProgressTracker::PassedTime(Timer *tim) {
  * is non-zero (i.e. if D is returned, HH, MM and SS will always
  * be returned as well). If all values are zero, then "0s" is
  * returned.
+ *
+ * msec:  Number of milliseconds representing the time period.
+ * color: Whether or not to print with colors and fancy formatting.
+ * lunit: Largest time unit to output.
  */
-string ProgressTracker::TimeString(const size_t sec) {
-    /*chrono::hours days   = chrono::duration_cast<hours>(sec) / 24;
-    chrono::hours hours  = chrono::duration_cast<hours>(sec - days*24);
-    chrono::minutes mins = chrono::duration_cast<minutes>(sec - days*24 - hours);
-    chrono::seconds secs = chrono::duration_cast<seconds>(sec - days*24 - hours - mins);*/
+string ProgressTracker::TimeString(const long long int msec, const bool color, const TimeUnit tu) {
+    long long int days, hours, mins, secs, msecs;
 
-    const slibreal_t
-        MINUTE = 60.0,
-        HOUR   = 60.0*MINUTE;
-        //DAY    = 24.0*HOUR;
+    const long long int
+        SECOND = 1000,
+        MINUTE = 60*SECOND,
+        HOUR   = 60*MINUTE,
+        DAY    = 24*HOUR;
 
-    //size_t days  = (size_t)(sec/DAY);
-    //size_t hours = (size_t)((sec - days*DAY)/HOUR);
-    //size_t mins  = (size_t)((sec - days*DAY - hours*HOUR)/MINUTE);
-    //size_t secs  = (size_t) (sec - days*DAY - hours*HOUR - mins*MINUTE);
-    size_t hours = (size_t)(sec/HOUR);
-    size_t mins  = (size_t)((sec - hours*HOUR)/MINUTE);
-    size_t secs  = (size_t) (sec - hours*HOUR - mins*MINUTE);
+    if (tu <= DAYS) days = msec/DAY;
+    else days = 0;
 
+    if (tu <= HOURS) hours = (msec - days*DAY)/HOUR;
+    else hours = 0;
+
+    if (tu <= MINUTES) mins = (msec - days*DAY - hours*HOUR)/MINUTE;
+    else mins = 0;
+
+    if (tu <= SECONDS) secs = (msec - days*DAY - hours*HOUR - mins*MINUTE)/SECOND;
+    else secs = 0;
+
+    if (tu < MILLISECONDS) msecs = (msec - days*DAY - hours*HOUR - mins*MINUTE - secs*SECOND);
+    else msecs = msec;
+
+    return FormattedTimeString(
+        days, hours, mins, secs, msecs, color
+    );
+}
+
+string ProgressTracker::FormattedTimeString(
+    const long long int days, const long long int hours, const long long int minutes,
+    const long long int seconds, const long long int milliseconds, const bool color
+) {
     ostringstream os;
     bool force = false;
+    const char *formatstring = "\x1B[1;33m";
 
-    /*if (days > 0) {
+    if (days > 0) {
         os << days << "d";
         force = true;
-    }*/
+    }
 
     if (force || hours > 0) {
+        if (color) os << formatstring;
+
         if (hours < 10 && force)
             os << "0";
 
-        os << hours << "h";
+        if (color)
+            os << hours << "\x1B[0mh";
+        else
+            os << hours << "h";
+
         force = true;
     }
     
     // MINUTES
-    if (force || mins > 0) {
-        if (mins < 10 && force)
+    if (force || minutes > 0) {
+        if (color) os << formatstring;
+
+        if (minutes < 10 && force)
             os << "0";
 
-        os << mins << "m";
+        if (color)
+            os << minutes << "\x1B[0mm";
+        else
+            os << minutes << "m";
+
+        force = true;
     }
 
     // SECONDS
-    if (secs < 10 && force)
-        os << "0";
-    
-    os << secs << "s";
+    if (force || seconds > 0) {
+        if (color) os << formatstring;
+
+        if (seconds < 10 && force)
+            os << "0";
+        
+        if (color)
+            os << seconds << "\x1B[0ms";
+        else
+            os << seconds << "s";
+    }
+
+    // MILLISECONDS
+    if (!force && seconds < 10) {
+        if (color) os << formatstring;
+
+        if (seconds > 0) {
+            if (milliseconds < 10)
+                os << "00";
+            else if (milliseconds < 100)
+                os << "0";
+        }
+
+        if (color)
+            os << milliseconds << "\x1B[0mms";
+        else
+            os << milliseconds << "ms";
+    }
 
     return os.str();
 }
